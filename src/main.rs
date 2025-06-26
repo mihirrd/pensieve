b6aa32f0602f202ed01aa7faeb8732d1ffa77a88
mihirrd <deshpande.mihir7@gmail.com>
2025-06-26 23:59:10 +0530

    Create http server with axum

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,43 @@
-mod store;
+use axum::{
+    routing::get,
+    routing::post,
+    Router,
+    response::Json,
+};
 
+use axum::extract::Path;
+use serde_json::{Value, json};
 
-fn main() {
+use serde::{Deserialize, Serialize};
 
-    let mut store = store::Store::<String, String>::new(4);
+#[derive(Serialize, Deserialize)]
+struct PutRequest {
+    key: String,
+    val: String
+}
+
+#[tokio::main(flavor = "current_thread")]
+async fn main() {
+    // build our application with a single route
+    let app = Router::new()
+        .route("/get/{key}",get(path))
+        .route("/post",post(put));
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:7878").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn path(Path(key): Path<u32>) -> Json<Value>{
+   Json(json!({ "data": key }))
+}
+
+async fn put(Json(payload): Json<PutRequest>) -> Json<Value> {
+    let key = payload.key;
+    let val = payload.val;
+
+    Json(json!({
+        "key" : key,
+        "val" : val
+    }))
 
 }
